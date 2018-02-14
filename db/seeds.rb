5ab21d568316304e25acaaba60043c17e050d2f0
Etienne Delorieux <etienne.delorieux@gmail.com>
2018-02-14 19:05:13 +0100

    Improve code after correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,12 @@ puts "Clearing database ..."
 Restaurant.destroy_all
 
 puts "Creating restaurants ..."
-CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
 
 10.times do
-  Restaurant.create(
+  Restaurant.create!(
     name: Faker::Coffee.blend_name,
     address: Faker::Address.street_address,
     phone_number: Faker::PhoneNumber.phone_number,
-    category: CATEGORIES.sample
+    category: Restaurant::CATEGORIES.sample
   )
 end
